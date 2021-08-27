b1b1bcf9cce0633170cc25c96115ee5ced7d802f
Atheelah <atheelahvanderschyff17@gmail.com>
2021-08-27 11:52:22 +0200

    update

--- app.py
+++ app.py
@@ -14,8 +14,16 @@ class User(object):
         self.password = password
 
 
+def dict_factory(cursor, row):
+    d = {}
+    for idx, col in enumerate(cursor.description):
+        d[col[0]] = row[idx]
+    return d
+
+
 def fetch_users():
     with sqlite3.connect('bookstore.db') as conn:
+        conn.row_factory = dict_factory
         cursor = conn.cursor()
         cursor.execute("SELECT * FROM user")
         users = cursor.fetchall()
